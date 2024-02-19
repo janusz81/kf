cd681678e010d094f5ff31e21ec156d8c0634dd5
janusz81 <160429487+janusz81@users.noreply.github.com>
2024-02-19 13:21:40 +0100

    Add files via upload

--- literowanie.py
+++ literowanie.py
@@ -1,5 +1,5 @@
 import random
-#import sys
+import sys
 #sys.stdout.reconfigure(encoding='utf-8')
 
 class Regula:
@@ -10,32 +10,45 @@ class Regula:
 	def __str__(self):
 		return "%s - %s - %s"%(self.znak,self.pol,self.ang)
 
-def zadanie(reguly,co,naco):
-	print("Zamiana %s na %s"%(co,naco))
-	los=random.sample(reguly,5)
-	pytanie=[getattr(l,co) for l in los]
-	odpowiedz=[getattr(l,naco) for l in los]
-	odpowiedz=" ".join(odpowiedz)
-	print("Zamień: %s"%(" ".join(pytanie)))
-	odp=input().strip()
-	if odp.lower().replace(" ","")==odpowiedz.lower().replace(" ",""):
-		print("OK\n")
-		return 1
-	else:
-		print("Prawidłowa: %s\n"%(odpowiedz))
-		return 0
+class Reguly:
+	def __init__(self):
+		self.reguly=[]
+	def readfromfile(self,filepath):
+		with open(filepath,"r",encoding="utf-8") as plik:
+			for linia in plik:
+				(znak,pol,ang)=linia.replace("\n","").split(";")
+				regula=Regula(znak,pol,ang)
+				self.reguly.append(regula)
+		if len(self.reguly)==0:
+			print("nie udało się wczytać definicji")
+			return None
+		else:return self
+	def zadanie(self,co,naco):
+		print("Zamiana %s na %s"%(co,naco))
+		los=random.sample(self.reguly,5)
+		pytanie=[getattr(l,co) for l in los]
+		odpowiedz=[getattr(l,naco) for l in los]
+		odpowiedz=" ".join(odpowiedz)
+		print("Zamień: %s"%(" ".join(pytanie)))
+		odp=input().strip()
+		if odp.lower().replace(" ","")==odpowiedz.lower().replace(" ",""):
+			print("OK\n")
+			return 1
+		else:
+			print("Prawidłowa: %s\n"%(odpowiedz))
+			return 0
 
-sciezka="literowanie.dat"
-reguly=[]
-with open(sciezka,"r",encoding="utf-8") as plik:
-	for linia in plik:
-		(znak,pol,ang)=linia.replace("\n","").split(";")
-		regula=Regula(znak,pol,ang)
-		reguly.append(regula)
-
-liczba_zadan=5
-liczba_poprawnych=0
-for i in range(liczba_zadan):
-	co,naco=random.sample(["znak","pol","ang"],2)
-	liczba_poprawnych+=zadanie(reguly,co,naco)
-print("Poprawnie %d z %s zadań."%(liczba_poprawnych,liczba_zadan))
+if __name__=="__main__":
+	if len(sys.argv)!=3:
+		print("Parametry: nazwa pliku z definicją i ilość zadań")
+		sys.exit(-1)
+	reguly=Reguly()
+	print(sys.argv[1])
+	ret=reguly.readfromfile(sys.argv[1])
+	if ret==None:sys.exit(-1)
+	liczba_zadan=int(sys.argv[2])
+	liczba_poprawnych=0
+	for i in range(liczba_zadan):
+		co,naco=random.sample(["znak","pol","ang"],2)
+		liczba_poprawnych+=reguly.zadanie(co,naco)
+	print("Poprawnie %d z %s zadań."%(liczba_poprawnych,liczba_zadan))
