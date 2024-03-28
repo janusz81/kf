61d84b603b6ae07a99f4e159aad383ad8d756519
janusz81 <160429487+janusz81@users.noreply.github.com>
2024-03-28 10:45:36 +0100

    Update literowanie.py

--- literowanie.py
+++ literowanie.py
@@ -4,7 +4,7 @@ import functools
 #sys.stdout.reconfigure(encoding='utf-8')
 
 digits=("zero","0"),("jeden","1"),("dwa","2"),("trzy","3"),("cztery","4"),("pięć","5"),("sześć","6"),("siedem","7"),("osiem","8"),("dziewięć","9"),\
-	("zero","0"),("one","1"),("two","2"),("three","3"),("four","3"),("five","5"),("six","6"),("seven","7"),("eight","8"),("nine","9")
+	("zero","0"),("one","1"),("two","2"),("three","3"),("four","4"),("five","5"),("six","6"),("seven","7"),("eight","8"),("nine","9")
 def numberstranslate(tekst):
 	return functools.reduce(lambda a,kv:a.replace(*kv),digits,tekst)
 
